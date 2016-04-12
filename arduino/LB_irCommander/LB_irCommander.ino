b04f28a02b28fa6069efc2491fd7be91b7a8af6e
sokanno <kannoso@gmail.com>
2016-04-12 18:21:15 +0100

    Protocol changed to 29bit

--- arduino/LB_irCommander/LB_irCommander.ino
+++ arduino/LB_irCommander/LB_irCommander.ino
@@ -2,12 +2,13 @@
 
 IRsend irsend;
 
-char data[2];
-byte highOrder;
-byte lowOrder;
-int mergedNumber;
-int address;
-int value;
+byte data[4];
+// byte byteData[4];
+unsigned long mergedNumber;
+unsigned int id; // 8bit 0~255
+int op; // 2bit 0~3, 0[==] 1[!=] 2[<] 3[>]
+int address; // 3bit 0~7 
+unsigned long value; // 16bit 0~65535
 
 void setup() {
   Serial.begin(57600);
@@ -15,23 +16,57 @@ void setup() {
 
 void loop() {
   if(Serial.available() > 0){
-    Serial.readBytes(data, 2);
-    highOrder = data[0];
-    lowOrder = data[1];
-    mergedNumber = (highOrder << 8) + lowOrder;
-    // code below are just for monitoring 
-    address = mergedNumber >> 12;
-    value = mergedNumber - (address << 12);
+    Serial.readBytes(data, 4);
+    // for(int i=0; i<4; i++){
+    //   byteData[i] = data[i];
+    // }
+    unsigned long data0 = data[0];
+    unsigned long data1 = data[1];
+    unsigned long data2 = data[2];
+    unsigned long data3 = data[3];
+
+    mergedNumber = (data0 << 21) +
+                   (data1 << 16) +
+                   (data2 << 8) + 
+                   data3;
+
+    // code below are just for monitoring
+    // id = byteData[0];
+    // op = byteData[1] >> 3; 
+    // address = byteData[1] - (op << 3);
+    // value = (byteData[2] << 8) + byteData[3];
+    id = data0;
+    op = data1 >> 3; 
+    address = data1 - (op << 3);
+    value = (data2 << 8) + data3;
+
+    
+    // Serial.println(mergedNumber);
+    Serial.print(id);
+    Serial.print(' ');
+    Serial.print(op);
+    Serial.print(' ');
     Serial.print(address);
     Serial.print(' ');
-    Serial.println(value);
+    Serial.print(value);
+    Serial.print(' ');
+    Serial.print(data0);
+    Serial.print(' ');
+    Serial.print(data1);
+    Serial.print(' ');  
+    Serial.print(data2);
+    Serial.print(' ');
+    Serial.print(data3);
+    Serial.print(' ');
+    Serial.println(mergedNumber);
     sendSignal(mergedNumber);
   }
 }
 
-void sendSignal(int packet){
-  for (int i = 0; i < 5; i++) {
-    irsend.sendSony(packet, 15); 
-    delay(100);
-  }
+void sendSignal(unsigned long packet){
+  // for (int i = 0; i < 3; i++) {
+    // irsend.sendSony(packet, 29); 
+    irsend.sendSony(packet, 29);
+  //   delay(50);
+  // }
 }
