947d78f2f79244511e61cfae07320f2824f5be57
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-10-18 16:12:20 +0200

    Integrate Background in ofApp

--- laserBugsOFApp/src/ofApp.cpp
+++ laserBugsOFApp/src/ofApp.cpp
@@ -1,13 +1,20 @@
 #include "ofApp.h"
 
+namespace laserbugs{
+
 void ofApp::setup(){
 	cout << "LaserBugs says hello!" << endl;
+	
+	_background = make_shared<Background>();
+	_background->setup();
 }
 
 void ofApp::update(){
-
+	_background->update();
 }
 
 void ofApp::draw(){
-
+	_background->draw();
 }
+
+} // namespace laserbugs
--- laserBugsOFApp/src/ofApp.h
+++ laserBugsOFApp/src/ofApp.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include "ofMain.h"
+#include "Background.h"
+
+namespace laserbugs{
 
 class ofApp : public ofBaseApp{
 	public:
 		void setup();
 		void update();
 		void draw();
+	
+		shared_ptr<Background> _background;
 };
+
+} // namespace laserbugs
